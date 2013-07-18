99cf0c76a20892ee0ca253b3a70ff912c6fcb50e
allen nie <allen.nie@gmail.com>
2013-07-18 05:56:02 +0000

    this

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ var buffer new Buffer("I am a string", "utf-8")
 
 
 app.get('/', function(request, response) {
-  response.send(Buffer);
+  response.send('Hello World 2!');
 });
 
 var port = process.env.PORT || 5000;
