d0341e7b6b535b07559d67d8631543892bb4dd8c
allen nie <allen.nie@gmail.com>
2013-07-18 06:47:01 +0000

    last

--- web.js
+++ web.js
@@ -4,9 +4,14 @@ var app = express.createServer(express.logger());
 
 //var buffer new Buffer("I am a string", "utf-8")
 
+var file2str = function(filename){
+  fs.readFileSync(filename);
+  return "Hello World 2!";
+
+}
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(file2str("index.html"));
 });
 
 var port = process.env.PORT || 5000;
