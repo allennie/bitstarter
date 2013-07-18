10481d544ed1c22e78ecd2083645d3930bc9a907
allen nie <allen.nie@gmail.com>
2013-07-18 05:58:05 +0000

    this

--- web.js
+++ web.js
@@ -2,7 +2,7 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-var buffer new Buffer("I am a string", "utf-8")
+//var buffer new Buffer("I am a string", "utf-8")
 
 
 app.get('/', function(request, response) {
