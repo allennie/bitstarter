3bbf1c6bcb47f3e91269471e608eff9116850b56
allen nie <allen.nie@gmail.com>
2013-07-18 05:43:27 +0000

    modified

--- web.js
+++ web.js
@@ -2,8 +2,11 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var buffer new Buffer("I am a string", "utf-8")
+
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(Buffer);
 });
 
 var port = process.env.PORT || 5000;
